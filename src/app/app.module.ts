8dcc7b62d4439be6279824d087c413ef0785d56d
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-08-16 13:45:48 +0530

    feat: add node js to the project
    MEAN Course - Section 3 - Adding NodeJS to the Project;  Adding the Node Backend,  Adding the Express Framework,  Fetching Initial Posts,  Using the Angular HTTP Client,  Understanding CORS,  Adding the POST Backend Point,  Adding Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-create/post-list/post-list.component';
-import { PostsService } from './posts/post-create/posts.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -31,6 +31,7 @@ import { PostsService } from './posts/post-create/posts.service';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
