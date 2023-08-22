030354de1a1317edbec350fe64297730365a7c3d
Damith Samarasighe <108716149+DHS0004@users.noreply.github.com>
2023-08-22 20:01:06 +0530

    feat: Section 6
    Section 6 - Finishing the Image Validator

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 console.log("here 1");
 mongoose
   .connect(
-    "mongodb+srv://damith:0AOfzWdJ8EP2KEaa@cluster0.ia2wxzs.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://damith:4YO4eLkqKLhgS8Uc@cluster0.ia2wxzs.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
     console.log("Connected to database!");
--- src/app/posts/post-create/posts.service.ts
+++ src/app/posts/post-create/posts.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 import { Post } from './post.model';
-import { Router } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -39,7 +39,7 @@ export class PostsService {
 
   getPost(id: string) {
     return this.http.get<{ _id: string; title: string; content: string }>(
-      'http://localhost:3000/api/posts' + id
+      'http://localhost:3000/api/posts/' + id
     );
   }
 
@@ -62,7 +62,7 @@ export class PostsService {
   updatePost(id: string, title: string, content: string) {
     const post: Post = { id: id, title: title, content: content };
     this.http
-      .put('http://localhost:3000/api/posts' + id, post)
+      .put('http://localhost:3000/api/posts/' + id, post)
       .subscribe((response) => {
         const updatedPosts = [...this.posts];
         const oldPostIndex = updatedPosts.findIndex((p) => p.id === post.id);
